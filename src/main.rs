c3f9e4da230ff988e620e692135c173c7f95f3fb
BuyMyMojo <hello@buymymojo.net>
2022-07-14 20:11:10 +1000

    feat(src/main.rs): :sparkles: Add memory usage check
    Add memory usage check command and implement 512MB file size limit check for push command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+extern crate redis;
+extern crate pretty_bytes;
+
+use pretty_bytes::converter::convert;
 use clap::Parser;
 use redis::Commands;
 use std::fs;
@@ -19,7 +23,7 @@ enum SubCommands  {
    /// Clones repos
    #[clap(arg_required_else_help = true)]
    Clone {
-       /// The remote to clone
+       /// The output directory for the cloned file
        #[clap(value_parser)]
        out_path: String,
 
@@ -27,10 +31,10 @@ enum SubCommands  {
        #[clap(value_parser)]
        file_key: String,
    },
-   /// Pushes things
+   /// Pushes a file to redis
    #[clap(arg_required_else_help = true)]
    Push {
-       /// The remote to target
+       /// Path to file to push
        #[clap(value_parser)]
        file_path: String,
 
@@ -38,10 +42,17 @@ enum SubCommands  {
        #[clap(value_parser)]
        file_key: String,
    },
-   /// Dletes things
+   /// Dletes a file from the DB
    #[clap(arg_required_else_help = true)]
    Delete {
        /// The redis key to delete from the DB
+       #[clap(value_parser)]
+       file_key: String,
+   },
+   /// Get memory usage of specified file
+   #[clap(arg_required_else_help = true)]
+   Usage {
+       /// The redis key to get memory usage from the DB
        #[clap(value_parser)]
        file_key: String,
    },
@@ -61,6 +72,9 @@ fn main() {
         }
         SubCommands::Delete { file_key } => {
             delete_file(addr, file_key);
+        }
+        SubCommands::Usage { file_key } => {
+            file_memory_usage(addr, file_key);
         }
     }
 }
@@ -101,12 +115,21 @@ fn upload_file(redis_address: String, file_path: String, file_key: String) {
     let mut con = client.get_connection()
         .expect("Failed to connect to Redis");
 
+    let file_size = fs::metadata(&file_path).unwrap().len();
+
+    if file_size > 512000000 {
+        println!("File is too large to push to Redis. Max size is 512MB");
+        return;
+    }
+
     // Read the file's contents
-    let contents = fs::read_to_string(file_path)
-        .expect("Something went wrong reading the file");
+    let mut f = File::open(&file_path).expect("no file found");
+    let metadata = fs::metadata(&file_path).expect("unable to read metadata");
+    let mut buffer = vec![0; metadata.len() as usize];
+    f.read(&mut buffer).expect("buffer overflow");
 
     // Store the file in the DB
-    let _ : () = con.set(format!("RedisFS:{}", &file_key), contents)
+    let _ : () = con.set(format!("RedisFS:{}", &file_key), buffer)
         .expect("Failed to set key");
 
     println!("Upload complete. Saved as RedisFS:{}", &file_key);
@@ -127,4 +150,21 @@ fn delete_file(redis_address: String, file_key: String) {
         .expect("Failed to delete key");
 
         println!("Removed {} from DB", file_key);
+}
+
+/// Get memory usage of specified key
+fn file_memory_usage(redis_address: String, file_key: String) {
+    println!("Checking memory usage of RedisFS:{}", file_key);
+
+    // Connect to Redis
+    let client = redis::Client::open(redis_address)
+        .expect("Failed to create Redis client");
+    let mut con = client.get_connection()
+        .expect("Failed to connect to Redis");
+
+    // Store the file in the DB
+    let usage: f64 = redis::cmd("MEMORY").arg("USAGE").arg(format!("RedisFS:{}", file_key)).query(&mut con)
+        .expect("Unable to get memory usage");
+
+    println!("RedisFS:{} uses {}", file_key, convert(usage));
 }
